3fa6b49f28e40e2489519615ed958f2fb0c5f9a3
William Condori Quispe <williamccondori@outlook.com>
2021-12-07 22:48:15 -0500

    configure amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "gseg": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
